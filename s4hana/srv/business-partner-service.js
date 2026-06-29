af9cabfb9ee45ab370c00d73dccb78d07297662f
Prajnashree Pradhan <prajnashreepradhan21@gmail.com>
2026-06-29 11:50:57 +0000

    Added XSUAA

--- s4hana/srv/business-partner-service.js
+++ s4hana/srv/business-partner-service.js
@@ -1,16 +1,11 @@
+const cds = require('@sap/cds');
+
 module.exports = cds.service.impl(async function () {
 
-//   const bupa = await cds.connect.to('API_BUSINESS_PARTNER');
+    const bupa = await cds.connect.to('API_BUSINESS_PARTNER');
 
-//   this.on('READ', 'BusinessPartners',
-//     req => bupa.run(req.query));
-// });
+    this.on('READ', 'BusinessPartners', async (req) => {
+        return await bupa.run(req.query);
+    });
 
-const bupa = await cds.connect.to('API_BUSINESS_PARTNER');
-
-this.on('READ', 'BusinessPartners', async (req) => {
-  req.query.limit(10); // Limit the number of results to 10
-  return await bupa.run(req.query);
-})
 });
-
